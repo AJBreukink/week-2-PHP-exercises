00ab60e16b7d075cb7d8df960c3f4a6dda861145
Arend-Jan Breukink <35037565+AJBreukink@users.noreply.github.com>
2018-01-28 13:29:05 +0100

    added input form

--- phpexercisesjuly.php
+++ phpexercisesjuly.php
@@ -1,5 +1,5 @@
 <?php
-  function julyCheck ($input) {
+function julyCheck ($input) {
      if ($input == 'july' || $input == 'July') {
         print "It's July, so it can get really hot.";
      } else {
@@ -7,5 +7,12 @@
         }
 }
 
-julyCheck ('');
+if (!empty($_POST)):
+   julyCheck(htmlspecialchars($_POST["name"]));
+else:
 ?>
+    <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+        Month: <input type="text" name="name"><br>
+        <input type="submit">
+    </form>
+<?php endif; ?>
